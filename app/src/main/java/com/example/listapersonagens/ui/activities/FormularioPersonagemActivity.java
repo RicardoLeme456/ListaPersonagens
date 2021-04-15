c8ae317080803446d4c1e7ea242618ba0bd9686b
RicardoLeme456 <69600786+RicardoLeme456@users.noreply.github.com>
2021-04-15 11:29:57 -0300

    Update

--- app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
@@ -1,12 +1,12 @@
 package com.example.listapersonagens.ui.activities;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.listapersonagens.R;
 import com.example.listapersonagens.dao.PersonagemDAO;
